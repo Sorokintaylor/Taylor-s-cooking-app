8218938683a224f9a6bb5d140ea09707a60ef128
Taylor <sorokintaylor@gmailcom>
2021-07-28 12:56:15 -0400

    Initial Commit

---
+++ Taylor's cooking app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taylor's cooking app
+//
+//  Created by Taylor Sorokin on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
